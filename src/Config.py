be3c823ca165d5bbeff396b6c16cdaf831673562
pramukesh <canews.in@gmail.com>
2025-07-04 16:56:12 +0530

    v0.9.3(4700)

--- src/Config.py
+++ src/Config.py
@@ -13,8 +13,8 @@ import time
 class Config(object):
 
     def __init__(self, argv):
-        self.version = "0.9.2"
-        self.rev = 4650
+        self.version = "0.9.3"
+        self.rev = 4700
         self.argv = argv
         self.action = None
         self.test_parser = None
